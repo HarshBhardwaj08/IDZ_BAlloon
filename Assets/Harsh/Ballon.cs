b953a85f5920aa7ad83bfe3f69b22ba96a51a6d5
Harsh Bhardwaj <110498182+HarshBhardwaj08@users.noreply.github.com>
2023-12-08 19:14:50 +0530

    New Launch Physics

--- Assets/Harsh/Ballon.cs
+++ Assets/Harsh/Ballon.cs
@@ -7,12 +7,18 @@ public class Ballon : MonoBehaviour
 {  
     Animator animator;
     Vector2 difference = Vector2.zero;
+    Vector3 downDir;
+    Vector3 upDir;
+    Vector3 finalVector;
     public GameObject waterHolder;
     public GameObject Impactpoint;
     SpriteRenderer spriteRenderer;
-   
+    public float powerUp;
+    public bool isPowerUp;
+    Rigidbody2D rg2d;
     private void Awake()
     {
+        rg2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void Start()
@@ -23,27 +29,55 @@ public class Ballon : MonoBehaviour
          
     }
    
-    private void OnMouseDown()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition)- (Vector2)transform.position;
+        if(collision.gameObject.tag == "Ballon")
+        {
+           
+             if(isPowerUp == true)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * powerUp, ForceMode2D.Impulse);
+            }
+           
+        }
+    }
 
+    private void OnMouseDown()
+    {   
+        Vector3 finalVector = (downDir - upDir).normalized;
+        isPowerUp = true;
+        difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition)- (Vector2)transform.position;
+        downDir = transform.position;
+       
     }
     private void OnMouseDrag()
     {
+        
+        isPowerUp = true;
         transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+      
     }
     private void OnMouseUp()
     {
+        
+     
         animator.SetTrigger("Burst");
+        isPowerUp = false;
+        upDir = transform.position;
+       
+        finalVector = (upDir - downDir).normalized;
+        Debug.Log(finalVector);
+        rg2d.AddForce((finalVector) * powerUp, ForceMode2D.Impulse);
     }
 
     public void BallonDestroy()
     {   
         Destroy(this.gameObject);
+       
         waterHolder.transform.parent = null;
         waterHolder.SetActive(true);
        
     }
-
+    
     
 }
--- Assets/Harsh/Boundries.cs
+++ Assets/Harsh/Boundries.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 public class Boundries : MonoBehaviour
 {
     public GameObject wall;
-    public float boundaryX = 10f; // Adjust this value based on your scene dimensions
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag != "Wall")
-        {   
-          
-            collision.gameObject.transform.localPosition = wall.transform.localPosition;
+        {
+            Vector3 TargetPos = collision.gameObject.transform.position;
+             TargetPos.x = wall.transform.position.x;
+            collision.gameObject.transform.position = TargetPos;
         }
-       
-        
     }
 
-    
+
 }
